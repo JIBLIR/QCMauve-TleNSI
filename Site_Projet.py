2131e767642ef9dd589df7ad0fff9900ecdf9cd8
Rouliaux <jules.ple@outlook.com>
2026-04-18 23:03:36 +0200

    Add QCM creation and deletion functionality

--- Site_Projet.py
+++ Site_Projet.py
@@ -125,6 +125,126 @@ def terminale():
 
 terminale.exposed = True 
 
+#============= Ajout de Qcm =============#
+def ajout_qcm(question=None,rep1=None,rep2=None,rep3=None,rep4=None,bonne_rep=None,matiere=None):
+
+    if "role" not in cherrypy.session:
+        raise cherrypy.HTTPRedirect("/connection")
+
+    if cherrypy.session["role"] == 0:
+        raise cherrypy.HTTPRedirect("/")
+
+    if rep1 and rep2 and rep3 and rep4 and question :
+        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password = "nsi", database="P02QCM")
+        cur = baseDeDonnees.cursor()
+        cur.execute(
+        "INSERT INTO questions (question,reponse1,reponse2,reponse3,reponse4,bonne_reponse,matiere) VALUES (%s,%s,%s,%s,%s,%s,%s)",(question,rep1,rep2,rep3,rep4,bonne_rep,matiere)
+        )
+        baseDeDonnees.commit()
+        cur.close()
+        baseDeDonnees.close()
+		
+    return """
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Ajouter QCM</title>
+		<link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+	</head>
+	<body>
+		<h1>Ajouter un QCM</h1>
+        
+		""" + menu() + """
+        
+        <form method="post" action="/ajout_qcm">
+			<input type="text" name="question" placeholder="Question"  required>
+			<input type="text" name="rep1" placeholder="Réponse"  required>
+			<input type="text" name="rep2" placeholder="Réponse"  required>
+            <input type="text" name="rep3" placeholder="Réponse"  required>
+            <input type="text" name="rep4" placeholder="Réponse"  required>
+            <input type="number" name="bonne_rep" min="1" max="4"  required>
+            <input type="text" name="matiere" placeholder="Matière"  required>
+		<button type="submit">Valider</button>
+		</form>
+	</body>
+</html>
+"""
+ajout_qcm.exposed = True
+
+#============= Suppression =============#
+
+def sup(id=None,question=None):
+    
+    if "role" not in cherrypy.session:
+        raise cherrypy.HTTPRedirect("/connection")
+
+    if cherrypy.session["role"] == 0:
+        raise cherrypy.HTTPRedirect("/")
+
+    liste_questions = ""
+    message = ""
+	
+    if question : 
+        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password = "nsi", database="P02QCM")
+        cur = baseDeDonnees.cursor()
+        cur.execute("SELECT id_question, question FROM questions WHERE question LIKE %s",("%" + question + "%",))
+        resultat = cur.fetchall()
+        
+        if resultat : 
+            for liste in resultat:
+                    id_question = liste[0]
+                    question_l = liste[1]
+                    
+                    liste_questions += """
+                    <p>
+                    """ + str(id_question) + """ - """ + question_l + """
+                    <a href="/sup?id=""" + str(id_question) + """">Supprimer</a>
+                    </p>
+                """
+        else:
+            message = "<p>Aucune question trouvée.</p>"
+        cur.close()
+        baseDeDonnees.close()
+    
+    if id : 
+        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password = "nsi", database="P02QCM")
+        cur = baseDeDonnees.cursor()
+        cur.execute("DELETE FROM questions WHERE id_question = %s",(id,))
+        baseDeDonnees.commit()
+
+        message = "<p>Question supprimée.</p>"
+
+        cur.close()
+        baseDeDonnees.close()
+
+    return """
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Ajouter QCM</title>
+		<link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+	</head>
+	<body>
+		<h1>Ajouter un QCM</h1>
+        
+		""" + menu() + """
+        
+        """ + message + """
+        
+        """ + liste_questions + """
+        
+		<form method="post" action="/sup">
+    		<input type="text" name="question" placeholder="Chercher une question à supprimer" required>
+    		<button type="submit">Valider</button>
+		</form>
+        
+	</body>
+</html>
+"""
+sup.exposed = True
+
 #============= Connection =============#
 def connection(identifiant=None, Mot_de_passe=None):
 
@@ -137,16 +257,18 @@ def connection(identifiant=None, Mot_de_passe=None):
             cur = baseDeDonnees.cursor()
 
             cur.execute(
-                "SELECT mdp_user FROM utilisateur WHERE nom_user = %s",(identifiant,)
+                "SELECT mdp_user, metier FROM utilisateur WHERE nom_user = %s",(identifiant,)
             )
 
             resultat = cur.fetchone()
 
             if resultat:
                 mdp_stocke = resultat[0]
+                metier = resultat[1]
 
                 if bcrypt.checkpw(Mot_de_passe.encode(), mdp_stocke):
                     cherrypy.session["user"] = identifiant
+                    cherrypy.session["role"] = metier
                     raise cherrypy.HTTPRedirect("/")
                 else:
                     print("Mot de passe incorrect")
@@ -196,18 +318,26 @@ def deconnexion():
 
 deconnexion.exposed = True
 
-=============# Creation de compte =============#
+#============= Creation de compte =============#
 def creer_compte(Profession = None,identifiant = None,Mot_de_passe = None):
 
     if Profession and identifiant and Mot_de_passe :
         Profession = int(Profession)
-        mot_de_passe_hash = bcrypt.hashpw(Mot_de_passe.encode(), bcrypt.gensalt())
         baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password = "nsi", database="P02QCM")
         cur = baseDeDonnees.cursor()
         cur.execute(
-        "INSERT INTO utilisateur (nom_user,mdp_user,metier) VALUES (%s,%s,%s)",(identifiant,mot_de_passe_hash,Profession)
-        )
-        baseDeDonnees.commit()
+            "SELECT * from utilisateur WHERE nom_user = %s",(identifiant,)
+		)
+        result = cur.fetchall
+        if result :
+            message = "<p style='color:red;'>Identifiant déjà pris</p>"
+        else :
+            mot_de_passe_hash = bcrypt.hashpw(Mot_de_passe.encode(), bcrypt.gensalt())
+            cur.execute(
+            "INSERT INTO utilisateur (nom_user,mdp_user,metier) VALUES (%s,%s,%s)",(identifiant,mot_de_passe_hash,Profession)
+            )
+            baseDeDonnees.commit()
+            message = "<p style='color:green;'>Compte créé avec succès</p>"
         cur.close()
         baseDeDonnees.close()
     return"""
@@ -235,6 +365,9 @@ def creer_compte(Profession = None,identifiant = None,Mot_de_passe = None):
 			
 			<button type="submit">Valider</button>
 			</form>
+            
+            """ + message + """
+            
 			<p > <a href="/connection">Se connecter </a> </p>
 		</div>
 	</body>
@@ -250,7 +383,7 @@ creer_compte.exposed = True
 #ip lycée : 172.16.100.22
 
 cherrypy.config.update({
-	"server.socket_host"	:"172.16.100.22", 
+	"server.socket_host"	:"192.168.1.8", 
 	"server.socket_port"	:5432,
 	"server.socket_pool"	:5,
 	"tools.sessions.on"		:True,
@@ -263,6 +396,8 @@ cherrypy.tree.mount(index, "/")
 cherrypy.tree.mount(seconde, "/seconde")
 cherrypy.tree.mount(premiere, "/premiere")
 cherrypy.tree.mount(terminale, "/terminale")
+cherrypy.tree.mount(ajout_qcm, "/ajout_qcm")
+cherrypy.tree.mount(sup, "/sup")
 cherrypy.tree.mount(connection, "/connection")
 cherrypy.tree.mount(deconnexion, "/deconnexion")
 cherrypy.tree.mount(creer_compte, "/creer_compte")
