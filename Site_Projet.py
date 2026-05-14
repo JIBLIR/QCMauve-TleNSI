8602c19671d42b466ec37e705254d7474e34b93e
Irdel Jean-Baptiste <157957198+JIBLIR@users.noreply.github.com>
2026-05-14 21:48:21 +0200

    Update Site_Projet.py

--- Site_Projet.py
+++ Site_Projet.py
@@ -1,54 +1,65 @@
 import cherrypy
 import os
 import mysql.connector
-import bcrypt # ---> Biblio pour le cryptage de mdp
+import bcrypt  # ---> Biblio pour le cryptage de mdp
 
-#=============Site WEB=============#
+
+# =============Site WEB=============#
 def menu():
     if "user" in cherrypy.session:
-        bouton = '<li id="co"><a href="/deconnexion">Se déconnecter</a></li>'
+        bouton = '<li><a href="/ajout_qcm">Ajout Qcm</a></li> <li><a href="/sup">Suprimmer un Qcm</a></li> <li id="co"><a href="/deconnexion">Se déconnecter</a></li>'
+        statut = '🧑‍🏫 Accès Professeur'
     else:
         bouton = '<li id="co"><a href="/connection">Se connecter</a></li>'
+        statut = '📖 Accès Etudiant'
 
     return """
     <div id="nav">
         <ul>
             <li><a href="/">Accueil</a></li>
             <li>Classe
-                <ul>
+                
                     <li><a href="/seconde">Seconde</a></li>
                     <li><a href="/premiere">Première</a></li>
                     <li><a href="/terminale">Terminale</a></li>
-                </ul>
+                
             </li>
-			<li><a href="/ajout_qcm">Ajout Qcm</a></li>
-            <li><a href="/sup">Suprimmer un Qcm</a></li>
-            """ + bouton + """
+            """ + bouton + statut + """
         </ul>
     </div>
     """
 
+
 def index():
+    if "user" in cherrypy.session:
+        message = '<img src="https://images7.memedroid.com/images/UPLOADED844/59be7d31cb08b.jpeg">' + '<br> <i>Vous pouvez désormais ajouter et supprimer des QCMs</i>'
+    else:
+        message = '<img src="https://storage.googleapis.com/proudcity/elgl/uploads/2023/04/yoda.jpg" alt="alternatetext">' + "<br> <i>Bonjour, vous voilà sur une page web où l'on peut reviser plusieures matières de différentes classes.</i>"
     return """
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <title>Mon site Web</title>
-        <link rel="stylesheet" href="/style_projet_Bd_NSI.css">
-    </head>
-    <body>
-        <h1>Site pour faire des qcm</h1>
-        
-        """ + menu() + """
+    <meta charset="utf-8">
+    <title>Mon site Web</title>
 
-        <p>Bonjour, vous voilà sur une page web où l'on peut reviser plusieures matiéres de différent classes.</p>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100..800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+</head>
+    <body>
+        <h1>QCMauve</h1>
+        
+        """ + menu() + message + """
     </body>
 </html>
 
 	"""
 
+
 index.exposed = True
+
 
 def seconde(reponse=None):
     import mysql.connector
@@ -69,7 +80,6 @@ def seconde(reponse=None):
             message = f'faux, {question[int(question[6]) + 1]}'
     cherrypy.session['index_question'] = index_question
 
-
     baseDeDonnees.close()
 
     return """
@@ -78,10 +88,10 @@ def seconde(reponse=None):
 	<head>
 		<meta charset="utf-8">
 		<title>Mon site Web</title>
-		<link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+        <link rel="stylesheet" href="/static/style_projet_Bd_NSI.css">
 	</head>
 	<body>
-		<h1>Site pour faire des qcm</h1>
+		<h1>QCMauve</h1>
 
 		""" + menu() + f""" {question[1]}
     <form method="post" action="/seconde">
@@ -101,7 +111,10 @@ def seconde(reponse=None):
     </body>
 </html>
     """
+
+
 seconde.exposed = True
+
 
 def premiere():
     return """
@@ -110,10 +123,10 @@ def premiere():
 	<head>
 		<meta charset="utf-8">
 		<title>Mon site Web</title>
-		<link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+        <link rel="stylesheet" href="/static/style_projet_Bd_NSI.css">
 	</head>
 	<body>
-		<h1>Site pour faire des qcm</h1>
+		<h1>QCMauve</h1>
 
 		""" + menu() + """
         
@@ -123,7 +136,9 @@ def premiere():
 
 	"""
 
+
 premiere.exposed = True
+
 
 def terminale():
     return """
@@ -132,10 +147,10 @@ def terminale():
 	<head>
 		<meta charset="utf-8">
 		<title>Mon site Web</title>
-		<link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+        <link rel="stylesheet" href="/static/style_projet_Bd_NSI.css">
 	</head>
 	<body>
-		<h1>Site pour faire des qcm</h1>
+		<h1>QCMauve</h1>
 
 		""" + menu() + """
         
@@ -145,22 +160,24 @@ def terminale():
 
 	"""
 
+
 terminale.exposed = True
 
-#============= Ajout de Qcm =============#
-def ajout_qcm(question=None,rep1=None,rep2=None,rep3=None,rep4=None,bonne_rep=None,matiere=None):
 
+# ============= Ajout de Qcm =============#
+def ajout_qcm(question=None, rep1=None, rep2=None, rep3=None, rep4=None, bonne_rep=None, matiere=None):
     if "role" not in cherrypy.session:
         raise cherrypy.HTTPRedirect("/connection")
 
     if cherrypy.session["role"] == 0:
         raise cherrypy.HTTPRedirect("/")
 
-    if rep1 and rep2 and rep3 and rep4 and question :
-        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password = "nsi", database="P02QCM")
+    if rep1 and rep2 and rep3 and rep4 and question:
+        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password="nsi", database="P02QCM")
         cur = baseDeDonnees.cursor()
         cur.execute(
-        "INSERT INTO questions (question,reponse1,reponse2,reponse3,reponse4,bonne_reponse,matiere) VALUES (%s,%s,%s,%s,%s,%s,%s)",(question,rep1,rep2,rep3,rep4,bonne_rep,matiere)
+            "INSERT INTO questions (question,reponse1,reponse2,reponse3,reponse4,bonne_reponse,matiere) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+            (question, rep1, rep2, rep3, rep4, bonne_rep, matiere)
         )
         baseDeDonnees.commit()
         cur.close()
@@ -172,7 +189,7 @@ def ajout_qcm(question=None,rep1=None,rep2=None,rep3=None,rep4=None,bonne_rep=No
 	<head>
 		<meta charset="utf-8">
 		<title>Ajouter QCM</title>
-		<link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+        <link rel="stylesheet" href="/static/style_projet_Bd_NSI.css">
 	</head>
 	<body>
 		<h1>Ajouter un QCM</h1>
@@ -192,12 +209,14 @@ def ajout_qcm(question=None,rep1=None,rep2=None,rep3=None,rep4=None,bonne_rep=No
 	</body>
 </html>
 """
+
+
 ajout_qcm.exposed = True
 
-#============= Suppression =============#
 
-def sup(id=None,question=None):
+# ============= Suppression =============#
 
+def sup(id=None, question=None):
     if "role" not in cherrypy.session:
         raise cherrypy.HTTPRedirect("/connection")
 
@@ -207,18 +226,18 @@ def sup(id=None,question=None):
     liste_questions = ""
     message = ""
 
-    if question :
-        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password = "nsi", database="P02QCM")
+    if question:
+        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password="nsi", database="P02QCM")
         cur = baseDeDonnees.cursor()
-        cur.execute("SELECT id_question, question FROM questions WHERE question LIKE %s",("%" + question + "%",))
+        cur.execute("SELECT id_question, question FROM questions WHERE question LIKE %s", ("%" + question + "%",))
         resultat = cur.fetchall()
 
-        if resultat :
+        if resultat:
             for liste in resultat:
-                    id_question = liste[0]
-                    question_l = liste[1]
+                id_question = liste[0]
+                question_l = liste[1]
 
-                    liste_questions += """
+                liste_questions += """
                     <p>
                     """ + str(id_question) + """ - """ + question_l + """
                     <a href="/sup?id=""" + str(id_question) + """">Supprimer</a>
@@ -229,10 +248,10 @@ def sup(id=None,question=None):
         cur.close()
         baseDeDonnees.close()
 
-    if id :
-        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password = "nsi", database="P02QCM")
+    if id:
+        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password="nsi", database="P02QCM")
         cur = baseDeDonnees.cursor()
-        cur.execute("DELETE FROM questions WHERE id_question = %s",(id,))
+        cur.execute("DELETE FROM questions WHERE id_question = %s", (id,))
         baseDeDonnees.commit()
 
         message = "<p>Question supprimée.</p>"
@@ -246,7 +265,7 @@ def sup(id=None,question=None):
 	<head>
 		<meta charset="utf-8">
 		<title>Ajouter QCM</title>
-		<link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+        <link rel="stylesheet" href="/static/style_projet_Bd_NSI.css">
 	</head>
 	<body>
 		<h1>Ajouter un QCM</h1>
@@ -265,21 +284,23 @@ def sup(id=None,question=None):
 	</body>
 </html>
 """
+
+
 sup.exposed = True
 
-#============= Connection =============#
-def connection(identifiant=None, Mot_de_passe=None):
 
+# ============= Connection =============#
+def connection(identifiant=None, Mot_de_passe=None):
     if identifiant and Mot_de_passe:
 
         try:
-            baseDeDonnees = mysql.connector.connect(host="localhost",user="nsi",password="nsi",database="P02QCM"
-            )
+            baseDeDonnees = mysql.connector.connect(host="localhost", user="nsi", password="nsi", database="P02QCM"
+                                                    )
 
             cur = baseDeDonnees.cursor()
 
             cur.execute(
-                "SELECT mdp_user, metier FROM utilisateur WHERE nom_user = %s",(identifiant,)
+                "SELECT mdp_user, metier FROM utilisateur WHERE nom_user = %s", (identifiant,)
             )
 
             resultat = cur.fetchone()
@@ -287,13 +308,18 @@ def connection(identifiant=None, Mot_de_passe=None):
             if resultat:
                 mdp_stocke = resultat[0]
                 metier = resultat[1]
-                # ERREUR : SI LE MDP EST FAUX
-                if bcrypt.checkpw(Mot_de_passe.encode(), mdp_stocke.encode()):
-                    cherrypy.session["user"] = identifiant
-                    cherrypy.session["role"] = metier
-                    raise cherrypy.HTTPRedirect("/")
-                else:
-                    print("Mot de passe incorrect")
+                try:
+                    if bcrypt.checkpw(Mot_de_passe.encode(), mdp_stocke.encode()):
+                        cherrypy.session["user"] = identifiant
+                        cherrypy.session["role"] = metier
+                        raise cherrypy.HTTPRedirect("/")
+                    else:
+                        message = "<p style='color:red;'>Mot de passe incorrect</p>"
+                except cherrypy.HTTPRedirect:
+                    raise
+                except Exception as e:
+                    message = "<p style='color:red;'>Erreur lors de la connexion</p>"
+                    print(f"Erreur : {e}")
 
             else:
                 print("Utilisateur inconnu")
@@ -301,21 +327,21 @@ def connection(identifiant=None, Mot_de_passe=None):
             cur.close()
             baseDeDonnees.close()
 
-        except mysql.connector.Error :
+        except mysql.connector.Error:
             print("Erreur base de données")
 
-    return"""
+    return """
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<title>Mon site Web</title>
-		<link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+        <link rel="stylesheet" href="/static/style_projet_Bd_NSI.css">
 	</head>
 	<body>
-		<h1>Site pour faire des qcm</h1>
+		<h1>QCMauve</h1>
 		
-		""" + menu() + """
+		""" + menu() + '<img src = "https://preview.redd.it/star-wars-death-star-2560x1440-v0-2081l20zh4by.png?auto=webp&s=7f0aa73f483d647681238ea2c5ce169f4db24604" width="1500" >' + """
         
 		<div id="inter_co">  
 			<p style="text-decoration: underline">Se connecter</p>
@@ -331,32 +357,37 @@ def connection(identifiant=None, Mot_de_passe=None):
 
 	"""
 
+
 connection.exposed = True
 
-#============= DECONNEXION =============#
+
+# ============= DECONNEXION =============#
 def deconnexion():
     cherrypy.session.pop("user", None)
     raise cherrypy.HTTPRedirect("/")
 
+
 deconnexion.exposed = True
 
-#============= Creation de compte =============#
-def creer_compte(Profession = None,identifiant = None,Mot_de_passe = None):
+
+# ============= Creation de compte =============#
+def creer_compte(Profession=None, identifiant=None, Mot_de_passe=None):
     message = ""
-    if Profession and identifiant and Mot_de_passe :
+    if Profession and identifiant and Mot_de_passe:
         Profession = int(Profession)
-        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password = "nsi", database="P02QCM")
+        baseDeDonnees = mysql.connector.connect(host="localhost", user='nsi', password="nsi", database="P02QCM")
         cur = baseDeDonnees.cursor()
         cur.execute(
-            "SELECT * from utilisateur WHERE nom_user = %s",(identifiant,)
-		)
+            "SELECT * from utilisateur WHERE nom_user = %s", (identifiant,)
+        )
         result = cur.fetchall()
-        if result :
+        if result:
             message = "<p style='color:red;'>Identifiant déjà pris</p>"
-        else :
+        else:
             mot_de_passe_hash = bcrypt.hashpw(Mot_de_passe.encode(), bcrypt.gensalt())
             cur.execute(
-            "INSERT INTO utilisateur (nom_user,mdp_user,metier) VALUES (%s,%s,%s)",(identifiant,mot_de_passe_hash,Profession)
+                "INSERT INTO utilisateur (nom_user,mdp_user,metier) VALUES (%s,%s,%s)",
+                (identifiant, mot_de_passe_hash, Profession)
             )
             baseDeDonnees.commit()
             message = "<p style='color:green;'>Compte créé avec succès</p>"
@@ -368,13 +399,12 @@ def creer_compte(Profession = None,identifiant = None,Mot_de_passe = None):
                 <head>
                     <meta charset="utf-8">
                     <title>Mon site Web</title>
-                    <link rel="stylesheet" href="/style_projet_Bd_NSI.css">
+                    <link rel="stylesheet" href="/static/style_projet_Bd_NSI.css">
                 </head>
                 <body>
-                    <h1>Site pour faire des qcm</h1>
+                    <h1>QCMauve</h1>
                     
-                    """ + menu() + """
-                    
+                    """ + menu() + '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvPZL842vqhNJ6p5MsIx3rHYoefjOyZczXTg&s" style="transform: scale(2); margin: 120px;"> <br> <i>pour rejoindre le côté obscur veillez créer un compte</i>' + """
                     <div id="inter_co">  
                     <p style="text-decoration: underline">Créer un compte</p>
                         <form method="post" action="/creer_compte">
@@ -394,22 +424,27 @@ def creer_compte(Profession = None,identifiant = None,Mot_de_passe = None):
                 </body>
             </html>"""
 
+
 creer_compte.exposed = True
 
-#=============Partie pour la mise en ligne du serveur=============#
+# =============Partie pour la mise en ligne du serveur=============#
 
-#ip perso : 192.168.1.8
-#ip lycée : 172.16.100.22
+# ip perso : 192.168.1.8
+# ip lycée : 172.16.100.22
 
 cherrypy.config.update({
-	"server.socket_host"	:"127.0.0.1",
-	"server.socket_port"	:5432,
-	"server.socket_pool"	:5,
-	"tools.sessions.on"		:True,
-	"tools.encode.encoding"	:"utf-8",
-	"tools.staticdir.on"	:True,
-	"tools.staticdir.dir": os.path.join(os.getcwd(), "static_2")
+    "server.socket_host": "127.0.0.1",
+    "server.socket_port": 8083,
+    "server.socket_pool": 5,
+    "tools.sessions.on": True,
+    "tools.encode.encoding": "utf-8",
+    "tools.staticdir.on": True,
+    "tools.staticdir.dir": os.path.join(os.path.dirname(__file__), "static_2")
 })
+
+# Monter le dossier static_2 sous /static pour servir les fichiers CSS/JS/images
+static_dir = os.path.join(os.path.dirname(__file__), "static_2")
+cherrypy.tree.mount(None, "/static", {'/': {'tools.staticdir.on': True, 'tools.staticdir.dir': static_dir}})
 
 cherrypy.tree.mount(index, "/")
 cherrypy.tree.mount(seconde, "/seconde")
@@ -420,8 +455,6 @@ cherrypy.tree.mount(sup, "/sup")
 cherrypy.tree.mount(connection, "/connection")
 cherrypy.tree.mount(deconnexion, "/deconnexion")
 cherrypy.tree.mount(creer_compte, "/creer_compte")
-
-
 
 cherrypy.engine.start()
 cherrypy.engine.block()
